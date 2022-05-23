4c3b9f4b86f35e3e6f38b055707054e90857b8c0
Ean <elvdv2017@gmail.com>
2022-05-23 09:13:36 +0200

    json level test\

---
+++ create_json.py
@@ -0,0 +1,96 @@
+
+#16x10
+
+level = [
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+]
+
+type_map = [
+    "ground-left-up",
+    "gound-up",
+    "ground-up-right",
+    "bridge-length-1",
+    "bridge-top",
+    "bridge-left",
+    "bridge-middle",
+    "bridge-right",
+    "ground-left",
+    "ground",
+    "ground-right",
+    "ground-all",
+    "ground-topright",
+    "egg-collectable",
+    "",
+    "",
+    "ground-left-down",
+    "ground-down",
+    "ground-down-right",
+    "ground-topleft",
+    "ground-bottomleft",
+    "ground-bottomright",
+    "",
+    "",
+    "start",
+    "button",
+    "pressureplate",
+    "door-closed",
+    "",
+    "",
+    "",
+    "",
+    "end",
+    "button-pressed",
+    "pressureplate-pressed",
+    "door-open",
+    "",
+    "",
+    "",
+    "",
+    "sign-arrow-bottomright",
+    "sign-arrow-topright",
+    "sign-arrow-topleft",
+    "sign-arrow-bottomleft",
+    "sign-arrow-down",
+    "",
+    "",
+    "",
+    "sign",
+    "sign-empty",
+    "sign-arrow-right",
+    "sign-arrow-left",
+    "sign-arrow-up",
+    "",
+    "",
+    "",
+    "water-anim-0",
+    "water-anim-1",
+    "water-anim-2",
+    "water-anim-3",
+    "water-still",
+    "",
+    "decoration7",
+    "decoration8",
+    ""
+]
+
+file = open("level0.json", "w")
+file.write("{\n")
+file.write("\t\"level-name\":\"Tutorial\",\n")
+file.write("\t\"level-map\":{\n")
+for i in range(len(level)):
+    if(i<len(level)-1):
+        file.write(f"\t\t\"{i}\":\"{type_map[level[i]]}\",\n")
+    else:
+        file.write(f"\t\t\"{i}\":\"{type_map[level[i]]}\"\n")
+file.write("\t}\n")
+file.write("}\n")
+file.close()
